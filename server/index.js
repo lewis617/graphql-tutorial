2109607696704a6776c63c12f79a00a253565c2a
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-19 19:00:01 +0800

    06-document

--- server/index.js
+++ server/index.js
@@ -2,14 +2,26 @@ const { ApolloServer, gql, MockList } = require('apollo-server');
 const casual = require('casual');
 
 const typeDefs = gql`
+  "用户类型"
   type User {
+    "用户ID"
     id: ID!
+    "用户名称"
     name: String!
+    "用户年龄"
     age: Int
+    """
+    是否单身
+    
+    单身可撩 or 名花（草）有主
+    """
     isSingle: Boolean
   }
   type Query {
-    user(name: String): User
+    user(
+      "参数：用户名称"
+      name: String
+      ): User
     users: [User]
   }
 `;
