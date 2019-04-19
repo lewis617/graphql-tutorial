d8785f80d09c839af488bcd93eee6eb420704b23
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-19 17:20:51 +0800

    03-基本类型与对象类型

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const users = [
+  { id: '1', name: '李雷', age: 12 },
+  { id: '2', name: '韩梅梅', isSingle: true },
+]
+
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    age: Int
+    isSingle: Boolean
+  }
   type Query {
-    hello: String
+    users: [User]
   }
 `;
 const resolvers = {
   Query: {
-    hello: () => 'hello world',
+    users: () => users,
   },
 };
 
