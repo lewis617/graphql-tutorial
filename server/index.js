3273258fb512cf0489029961ab675dc347252ef8
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-24 15:47:35 +0800

    bugfix

--- server/index.js
+++ server/index.js
@@ -48,15 +48,15 @@ const resolvers = {
   Mutation: {
     createUser: (parent, args) => {
       console.log('createUser args: ', args);
-      return { name: args.name };
+      return { name: args.user.name };
     },
     updateUser: (parent, args) => {
       console.log('updateUser args: ', args);
-      return { id: args.id };
+      return { id: args.user.id };
     },
     deleteUser: (parent, args) => {
       console.log('deleteUser args: ', args);
-      return { id: args.id };
+      return { id: args.user.id };
     },
   },
 };
