7b9dbe6a5895ba3cacb638d8dc1cae5c7d025153
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-24 15:56:30 +0800

    remove 复杂 mock

--- server/index.js
+++ server/index.js
@@ -1,23 +1,12 @@
-const { ApolloServer, MockList } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const casual = require('casual');
 const typeDefs = require('./types');
 const resolvers = require('./resolvers');
 
-const mocks = {
-  String: () => casual.word,
-  User: () => ({
-    name: () => casual.name,
-    age: () => casual.integer(0, 120),
-  }),
-  Query: () => ({
-    users: () => new MockList([0, 12]),
-  }),
-};
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  mocks,
+  mocks: { String: () => casual.word },
   mockEntireSchema: false,
   rootValue: { name: 'rootValue' },
 });
