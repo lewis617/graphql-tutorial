ae393578361b636ccda2709abe07637455387fcf
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-26 15:49:20 +0800

    添加菜单浮层

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
     "extends": "airbnb",
+    "parser": "babel-eslint",
     "rules": {
         "no-underscore-dangle": [0],
         "react/jsx-filename-extension": [0],
         "react/forbid-prop-types": [0],
+        "jsx-a11y/click-events-have-key-events": [0],
+        "jsx-a11y/no-static-element-interactions": [0],
     },
 };
--- client/layout/Nav/index.js
+++ client/layout/Nav/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Link } from 'umi';
 import styles from './index.less';
 
-const Nav = () => (
-  <div className={styles.nav}>
-    <Link to="/"><div className={styles.logo} /></Link>
-    <div className={styles.menu} />
-  </div>
-);
+class Nav extends PureComponent {
+  state = {
+    show: false,
+  }
+
+  toggleMenuContainer = () => this.setState(prev => ({ show: !prev.show }))
+
+  render() {
+    const { show } = this.state;
+    return (
+      <div>
+        <div className={styles.nav}>
+          <Link to="/"><div className={styles.logo} /></Link>
+          <div className={styles.menu} onClick={this.toggleMenuContainer} />
+        </div>
+        {show && (
+          <div className={styles.menuContainer}>
+            <div>
+              <div className={styles.close} onClick={this.toggleMenuContainer}>关闭</div>
+              <input className={styles.search} type="text" />
+            </div>
+          </div>
+        )}
+      </div>
+
+    );
+  }
+}
 
 export default Nav;
