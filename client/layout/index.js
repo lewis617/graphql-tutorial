cc1253423782f94c443ce9e659d7cb34e694e638
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-26 20:41:00 +0800

    错误处理 && 手动 query

--- client/layout/index.js
+++ client/layout/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from 'react-apollo';
+import { onError } from 'apollo-link-error';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Nav from './Nav';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/',
+});
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => toast.error(message));
+  }
+  if (networkError) { toast.error(networkError); }
+});
+
+const client = new ApolloClient({
+  link: errorLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 const Layout = ({ children, location }) => (
   <ApolloProvider client={client}>
     {location.pathname === '/login' ? null : <Nav />}
     {children}
+    <ToastContainer />
   </ApolloProvider>
 );
 Layout.propTypes = {
--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { router } from 'umi';
+import { ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+import { toast } from 'react-toastify';
 import styles from './index.less';
 
+const LOGIN = gql`
+query($user: LoginInput!){
+  login(user: $user){
+    token
+  }
+}
+`;
 class Login extends Component {
   state = {}
+
+  handleSubmit = async (client) => {
+    const name = this.nameInput.value;
+    const password = this.pswInput.value;
+    if (!name || !password) { toast.error('请填写用户名或者密码'); return; }
+    const { data: { login: { token } } } = await client.query({
+      query: LOGIN,
+      variables: { user: { name, password } },
+    });
+    window.localStorage.setItem('token', token);
+    router.goBack();
+  }
 
   render() {
     return (
@@ -16,10 +38,15 @@ class Login extends Component {
           <a target="_blank" rel="noopener noreferrer" href="https://accounts.douban.com/passport/agreement">豆瓣使用协议、隐私政策</a>
         </div>
         <div className={styles.formItems}>
-          <input type="text" name="name" placeholder="用户名" />
-          <input type="password" name="password" placeholder="密码" />
+          <input type="text" name="name" placeholder="用户名" ref={(el) => { this.nameInput = el; }} />
+          <input type="password" name="password" placeholder="密码" ref={(el) => { this.pswInput = el; }} />
         </div>
-        <div className={styles.submit}>登录</div>
+        <ApolloConsumer>
+          {client => (
+            <div className={styles.submit} onClick={() => this.handleSubmit(client)}>登录</div>
+          )}
+        </ApolloConsumer>
+
       </div>
     );
   }
