dd767487906067b5c141c4f8096468006ebb40ae
tianke.lyq <tianke.lyq@alibaba-inc.com>
2019-04-26 16:55:58 +0800

    完成登录静态页面

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -7,8 +7,19 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.loginContainer}>
         <div className={styles.loginCancel} onClick={() => router.goBack()} />
+        <div className={styles.title}>登录豆瓣</div>
+        <div className={styles.tips}>
+          登录注册表示同意
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://accounts.douban.com/passport/agreement">豆瓣使用协议、隐私政策</a>
+        </div>
+        <div className={styles.formItems}>
+          <input type="text" name="name" placeholder="用户名" />
+          <input type="password" name="password" placeholder="密码" />
+        </div>
+        <div className={styles.submit}>登录</div>
       </div>
     );
   }
